0cd7c5dcc4168ef0b047cf3d9e0113bd7aa7d074
fauzaanirsyaadi <fauzaan.irsyaadi@gmail.com>
2022-09-20 14:31:47 +0700

    lesson 7 : variable and data type

---
+++ fundamental/variables.py
@@ -0,0 +1,27 @@
+'''
+print('Hello World')
+'''
+x = 1             # x is an integer
+y = 2.5           # y is a float
+name = 'Fauzaan'  # name is a string
+is_cool = True    # is_cool is a boolean
+
+# check type
+print(type(x))
+
+
+# multiple assigment
+a, b, address, is_warm = (1, 2.5, 'Indonesia', False)
+
+print(a, b, address, is_warm)
+
+# Basic Math
+c = a + b
+
+print(c)
+
+# Casting
+c = str(c)
+
+# Check Type
+print('Check type of c is ', type(c))
